14e7661c02f3ed089ea99ccb4e50018227b8a7c3
Jorge Henrique <79412756+Melguis@users.noreply.github.com>
2022-04-30 16:05:50 -0300

    Add files via upload

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/card.css'
+
+export default function Card(props) {
+  return (
+    <div className="card">
+        <strong>{props.name}</strong>
+        <small>{props.time}</small>
+    </div>
+  )
+}
